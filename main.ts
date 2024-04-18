d305defba4d1ba783447c92ce768de57c8735bc2
GiminiKhan <160707616+GiminiKhan@users.noreply.github.com>
2024-04-18 21:00:42 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! usr/bin/env node
+
+// word counter
+
+// Printing a welcome message
+console.log("\n\tWelcome to \'Giminikhan\' - Word Counter in Typescript\n");
+
+import inquirer from "inquirer";
+
+const answer: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+      name: "Sentence",
+      type: "input",
+      message: "Enter your sentence to count the word:"
+    }
+])
+const words = answer.Sentence.trim().split(" ")
+console.log(`Your sentence word count is ${words.length}`)
